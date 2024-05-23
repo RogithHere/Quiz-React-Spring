8e7e17c84128d44330589e0016b44a2344f71b40
srihemainfo <developer@cwd.co.in>
2024-05-23 12:10:55 +0530

    Init

---
+++ src/main/java/com/rogith/QuizSpring/QuizSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rogith.QuizSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogith/QuizSpring/QuizSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogith.QuizSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
